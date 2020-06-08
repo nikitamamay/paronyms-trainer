861fdea4ddafb7b389c8bd8727a55862b779950f
Nikita Mamay <nikita@mamay.su>
2020-06-08 14:38:58 +0600

    Created answering system

--- script.js
+++ script.js
@@ -31,7 +31,47 @@ const uploadToStorage = () => {
 };
 
 
+
+const nextWord = () => {
+  if (wordsList.length > 0) {
+    let [ index, word_right ] = wordsList[0];
+    let meaning = paronyms[index][word_right];
+
+    meaningBox.innerHTML = Object.keys(meaning).join("<br>");
+    answersList.innerHTML = "";
+
+    for (let word of Object.keys(paronyms[index])) {
+      let onclick = () => {
+        let last = wordsList.shift();
+        if (word == word_right) {
+          console.log("right!");
+        } else {
+          wordsList.push(last);
+          console.log("wrong!")
+        }
+        uploadToStorage();
+        nextWord();
+      };
+
+      let item = Object.assign(document.createElement("li"), {
+        innerHTML: word,
+        onclick,
+      });
+
+      answersList.appendChild(item);
+    }
+  } else {
+    resetWordsList();
+    nextWord();
+  }
+};
+
+
 // ---
+
+let meaningBox = document.getElementById("meaning");
+let answersList = document.getElementById("answers_list");
+
 
 if (localStorage.getItem("paronyms") === null) {
   resetWordsList();
@@ -39,3 +79,5 @@ if (localStorage.getItem("paronyms") === null) {
 
 
 init();
+
+nextWord();
